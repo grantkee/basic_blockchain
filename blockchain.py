644e2536c0839b5cb19eed8d341471c4c6cf4c61
grantkee <grant.d.kee@gmail.com>
2021-01-28 22:01:33 -0600

    new_transaction def adds a transaction to the current list and returns the index of the block

--- blockchain.py
+++ blockchain.py
@@ -6,9 +6,22 @@ class Blockchain(object):
     # creates a new Block and adds it to the chain
     pass
   
-  def new_transaction(self):
-    # adds a new transaction to the list of transactions
-    pass
+  def new_transaction(self, sender, recipient, amount):
+    """
+    creates a new transaction to go into the next mined Block
+    :param sender: <str> address of the sender
+    :param recipient: <str> address of recipient
+    :param amount: <int> amount to send
+    :return: <int> the index of the Block that holds the transaction
+    """
+
+    self.current_transactions.append({
+      'sender': sender,
+      'recipient': recipient,
+      'amount': amount,
+    })
+
+    return self.last_block['index'] + 1
 
   @staticmethod
   def hash(block):
