3abf722de63606211c254e793b33ef86556e1d19
grantkee <grant.d.kee@gmail.com>
2021-01-30 12:25:01 -0600

    updated new_block method and hashing it with static method

--- blockchain.py
+++ blockchain.py
@@ -5,10 +5,31 @@ from time import time
 class Blockchain(object):
   def __init__(self):
     self.current_transactions = []
+    self.chain = []
 
-  def new_block(self):
-    # creates a new Block and adds it to the chain
-    pass
+    # create genesis block
+    self.new_block(previous_hash = 1, proof = 100)
+
+  def new_block(self, proof, previous_hash = None):
+    """
+      Create a new Block in the Blockchain
+      :param proof: <int> return from Proof of Work algorithm
+      :param previous_hash: (optional) <str> hash from previous block in chain - 1st block has none
+      :return: <dict> the new block
+    """
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'transactions': self.current_transactions,
+      'proof': proof,
+      'previous_hash': previous_hash or self.hash(self.chain[-1]),
+    }
+
+    # reset current list of transactions
+    self.current_transactions = []
+
+    self.chain.append(block)
+    return block
   
   def new_transaction(self, sender, recipient, amount):
     """
@@ -30,9 +51,16 @@ class Blockchain(object):
   @staticmethod
   def hash(block):
     # hashes a Block
-    pass
+    """
+      hash the block using SHA-256
+      :param block: <dict> Block
+      :return: <str> hashed string
+    """
+    # dictionary must be ordered for consistent hashes
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
 
   @property
   def last_block(self):
     # returns the last Block in the chain
-    pass
+    return self.chain[-1]
