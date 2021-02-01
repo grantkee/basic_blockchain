b23f21c8a37249195d03fc99a32d1c95752ba42e
grantkee <grant.d.kee@gmail.com>
2021-02-01 17:26:01 -0600

    added basic flask structure

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,10 @@
 import hashlib
 import json
-
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+from flask import Flask
 
 class Blockchain(object):
   def __init__(self):
@@ -93,3 +95,32 @@ class Blockchain(object):
   def last_block(self):
     # returns the last Block in the chain
     return self.chain[-1]
+
+# instantiate the node
+app = Flask(__name__)
+
+#generate universally unique address for the node
+node_identifier = str(uuid4()).replace('-', '')
+
+# instantiate the blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+  return "Mining a new Block"
+
+@app.rout('/transactions/new', methods=['POST'])
+def new_transaction():
+  return 'Adding a new transaction'
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+  response = {
+    'chain': blockchain.chain,
+    'length': len(blockchain.chain)
+  }
+  return jsonify(response), 200
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=4000)
+  
